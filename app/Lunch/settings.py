df8b8f7a6dd7e74b727637c72d2c6f201ed1829c
Jawaad Mahmood <jmahmood@salesforce.com>
2017-10-18 16:56:06 +0900

    Settings fix

--- app/Lunch/settings.py
+++ app/Lunch/settings.py
@@ -16,7 +16,7 @@ import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_ROOT = os.path.join(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../'), '../')
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -133,16 +133,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
-
 TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'),)
+
+# Extra places for collectstatic to find static files.
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
-
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 
 # Simplified static file serving.
