606370aa67f84ed0285df1fc228938c30619fcd5
Jawaad Mahmood <jmahmood@salesforce.com>
2017-10-19 21:36:17 +0900

    Provision DB just for testing?
    Signed-off-by: Jawaad Mahmood <jmahmood@salesforce.com>

--- app/Lunch/staging.py
+++ app/Lunch/staging.py
@@ -4,6 +4,9 @@ from Lunch.settings import *
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
+test_db_from_env = dj_database_url.config(env='HEROKU_POSTGRESQL_JADE_URL', conn_max_age=500)
+DATABASES['default']['test'].update(test_db_from_env)
+
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 ALLOWED_HOSTS = ['jbm-lunchforce-staging.herokuapp.com']
 SECURE_SSL_REDIRECT = True
