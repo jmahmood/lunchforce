7133b9a97dbf35deeb1b0084b4b8b58be456a932
Jawaad Mahmood <jmahmood@salesforce.com>
2017-10-19 23:30:30 +0900

    Why is it not loading the second DB name?
    Signed-off-by: Jawaad Mahmood <jmahmood@salesforce.com>

--- app/Lunch/herokuci.py
+++ app/Lunch/herokuci.py
@@ -3,7 +3,7 @@ from Lunch.settings import *
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-test_env_database_key = (k for k in os.environ.keys() if k.startswith('HEROKU_POSTGRESQL_') and k.endswith('_URL'))[0]
+test_env_database_key = [k for k in os.environ.keys() if k.startswith('HEROKU_POSTGRESQL_') and k.endswith('_URL')][0]
 print(test_env_database_key)
 test_db_from_env = dj_database_url.config(env=test_env_database_key, conn_max_age=500)
 DATABASES['default']['TEST'].update(test_db_from_env)
