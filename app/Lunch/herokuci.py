227e8bd1a2efca5c9607f7391d5f4151f6de846c
Jawaad Mahmood <jmahmood@salesforce.com>
2017-10-19 23:36:02 +0900

    Do we have to use KeepDB?
    Signed-off-by: Jawaad Mahmood <jmahmood@salesforce.com>

--- app/Lunch/herokuci.py
+++ app/Lunch/herokuci.py
@@ -3,12 +3,7 @@ from Lunch.settings import *
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-#test_env_database_key = [k for k in os.environ.keys() if k.startswith('HEROKU_POSTGRESQL_') and k.endswith('_URL')][0]
-#print(test_env_database_key)
-#test_db_from_env = dj_database_url.config(env=test_env_database_key, conn_max_age=500)
 DATABASES['default']['TEST'].update(db_from_env)
-print(DATABASES)
-print(os.environ)
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 ALLOWED_HOSTS = ['jbm-lunchforce-staging.herokuapp.com']
